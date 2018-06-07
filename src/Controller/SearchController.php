a12a6f88d199b27f510ce4f59ef3cde7fdda7062
Aurélien BLOT <aurelien.blot@gmail.com>
2018-06-07 16:52:08 +0200

    Update SearchController.php

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -18,19 +18,21 @@ class SearchController extends Controller
     {
         $countryRepo = $this->getDoctrine()->getRepository(Country::class);
 
-        $dist=null; $lat=null; $long=null; $dens=null; $tempAvg=null; $lang1=null; $lang2=null; $lang3=null ; $cont=null; $devise=null;
-/*
+        //$dist=null; $lat=null; $long=null; $dens=null; $tempAvg=null; $lang1=null; $lang2=null; $lang3=null ; $cont=null; $devise=null;
+
         $dist=$request->get('dist');
-        $lat=$request->get('lat');
-        $long=$request->get('long');
+        //$lat=$request->get('lat');
+        //$long=$request->get('long');
+        $lat = 48.864716;
+        $long= 2.349014;
         $tempAvg=$request->get('temp');
         $dens=$request->get('dens');
         $lang1=$request->get('lang1');
         $lang2=$request->get('lang2');
         $lang3=$request->get('lang3');
         $cont=$request->get('cont');
-        $devise=$request->get('dev');
-*/
+        $devise=$request->get('devise');
+
         $countries = $countryRepo->search($dist,$lat,$long, $dens, $tempAvg, $lang1,$lang2, $lang3, $cont, $devise );
         //$countries = $countryRepo->search();
 
